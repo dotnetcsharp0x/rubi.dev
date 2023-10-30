6b55a892b64a2cc7cfca1a829217c568ff381b46
rubi <admin@rubi.dev>
2023-10-30 10:50:02 +0300

    change button styles

--- src/components/Register/RegisterPage.tsx
+++ src/components/Register/RegisterPage.tsx
@@ -45,28 +45,16 @@ async function RegisterUser() {
   }
 }
 
-// async function loginUser(iuser:ILogin) {
-//   try {
-//     fetchToken();
-//     console.log(jwt);
-//     cookies.set("jwt",jwt)
-//   }
-//   catch (e) {
-//     alert(e)
-//   }
-// }
-
   return (
     <div className='grid place-items-center mt-10'>
     <form className="flex max-w-md flex-col gap-4 grid place-items-center" 
     >
       <img src="/logo512.png" className="h-20 w-20 mb-4 max-auto content-center" alt="Skyme logo" />
       <div>
-        <h1>{email}</h1>
         <div className="relative mb-1">
           <div className="absolute inset-y-0 left-0 z-10 flex items-center pl-3.5 pointer-events-none">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-slate-400">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
             </svg>
           </div>
         <input
@@ -76,16 +64,15 @@ async function RegisterUser() {
           type="text"
           value={name}
           className="z-0 bg-gray-700 text-slate-300 pl-12 rounded-md border-0 border-transparent focus:ring-0"
-          onChange={(e)=>setEmail(e.target.value)}
+          onChange={(e)=>setName(e.target.value)}
         />
         </div>
       </div>
       <div>
-        <h1>{email}</h1>
         <div className="relative mb-1">
           <div className="absolute inset-y-0 left-0 z-10 flex items-center pl-3.5 pointer-events-none">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-slate-400">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w- h-6 text-slate-400">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
             </svg>
           </div>
         <input
@@ -95,12 +82,11 @@ async function RegisterUser() {
           type="text"
           value={name2}
           className="z-0 bg-gray-700 text-slate-300 pl-12 rounded-md border-0 border-transparent focus:ring-0"
-          onChange={(e)=>setEmail(e.target.value)}
+          onChange={(e)=>setName2(e.target.value)}
         />
         </div>
       </div>
       <div>
-        <h1>{email}</h1>
         <div className="relative mb-1">
           <div className="absolute inset-y-0 left-0 z-10 flex items-center pl-3.5 pointer-events-none">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-slate-400">
@@ -146,9 +132,9 @@ async function RegisterUser() {
       </div>
       <div className='bg-slate-700 h-1 mt-2 border-separate rounded-md opacity-50'></div>
       </div>
-      <Button type="button" onClick={onButtonClick} className=''>
+      <button type="button" onClick={onButtonClick} className='bg-blue-800 hover:bg-blue-700 rounded-md py-3 px-5 text-slate-300'>
         Register
-      </Button>
+      </button>
     </form>
     </div>
   )
